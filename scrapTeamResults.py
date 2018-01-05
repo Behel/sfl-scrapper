5196508352d752d98dd963f85a1c46668145706e
Bastien Luneteau <bastien.luneteau@gmail.com>
2018-01-05 14:14:08 +0100

    scrap Team picks & results

---
+++ scrapTeamResults.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import requests, sys, getopt, time
+import urllib.request
+from util.html_table_parser import HTMLTableParser
+
+
+##############################
+# Récupération des arguments #
+##############################
+
+email = ''
+password = ''
+
+sys.argv.remove(sys.argv[0])
+try:
+    opts, args = getopt.getopt(sys.argv, 'he:p:')
+except getopt.GetoptError:
+    print('scrapTeamResults.py -e <email> -p <password> -d <date>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        print('scrapTeamResults.py -e <email> -p <password> -d <date>')
+        sys.exit()
+    elif opt == '-e':
+        email = arg
+    elif opt == '-p':
+        password = arg
+
+
+def main(_email, _password):
+
+    retour = ""
+
+    ####################
+    # Authentification #
+    ####################
+    session = requests.Session()
+    payload = {'email': _email, 'password': _password}
+    URLconnection = 'http://fantasy.sofoot.com/login.php'
+    r = session.post(URLconnection, data=payload)
+
+
+    #########################################
+    # Récupération des compétitions du jour #
+    #########################################
+
+    URLteam = 'http://fantasy.sofoot.com/?tpl=team'
+    ajaxTeam = session.get(URLteam)
+    soup = BeautifulSoup(ajaxTeam.content, "lxml")
+    teamLines = str(soup.find("table"))
+    p = HTMLTableParser()
+    p.feed(teamLines)
+
+    i=2
+    while i < len(p.tables[0]):
+        retour += p.tables[0][i][1]+","+p.tables[0][i][4]+"\n"
+        i+=1
+
+    print(retour)
+    return retour
+
+if __name__ == '__main__':
+    main(email, password)
+

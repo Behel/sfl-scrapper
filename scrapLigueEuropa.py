f61d1ec9260560678dd29f36251ab944b7b32748
Bastien Luneteau <bastien.luneteau@gmail.com>
2018-02-27 21:46:28 +0100

    LigueEuropa

---
+++ scrapLigueEuropa.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests, sys, getopt, time
+
+
+##############################
+# Récupération des arguments #
+##############################
+
+email = ''
+password = ''
+# Insérez ci-dessous le nom de votre équipe
+team_name="Brazilala"
+requestedDate = time.strftime("%Y-%m-%d")
+# Par défaut date du jour
+
+sys.argv.remove(sys.argv[0])
+try:
+    opts, args = getopt.getopt(sys.argv, 'he:p:d:')
+except getopt.GetoptError:
+    print('scrapLigueEuropa.py -e <email> -p <password>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt == '-h':
+        print('scrapLigueEuropa.py -e <email> -p <password>')
+        sys.exit()
+    elif opt == '-e':
+        email = arg
+    elif opt == '-p':
+        password = arg
+
+
+def main(_email, _password,_team_name):
+
+    retour = ""
+
+    ####################
+    # Authentification #
+    ####################
+    session = requests.Session()
+    payload = {'email': _email, 'password': _password, 'remember': 'on'}
+    URLconnection = 'http://fantasy.sofoot.com/login.php'
+    session.post(URLconnection, data=payload)
+
+
+    #########################################
+    # Récupération des compétitions du jour #
+    #########################################
+
+    URLEuropa = 'http://fantasy.sofoot.com/?tpl=group-europa'
+    ajaxMatches = session.get(URLEuropa)
+    soup = BeautifulSoup(ajaxMatches.content, "lxml")
+
+    matches_coupe = soup.find_all(class_="tournament-bracket__match")
+    for match in matches_coupe:
+        equipes_match = match.find_all(class_="tournament-bracket__code")
+        print(equipes_match)
+        if team_name == equipes_match[0].string or team_name == equipes_match[1].string:
+            score_match = match.find_all(class_="tournament-bracket__number")
+            retour = equipes_match[0].string + "," + score_match[0].string + '\n' + equipes_match[1].string + "," + score_match[1].string
+
+    print(retour)
+    return retour
+
+if __name__ == '__main__':
+    main(email, password, team_name)
+
